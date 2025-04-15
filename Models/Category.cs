aafd9e5db07ccb810f70fe2ddc60bf6be0a3034b
robsonlmds <YOUR_EMAIL>
2025-04-15 13:33:01 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Category.cs
@@ -0,0 +1,6 @@
+﻿namespace Planner.Models;
+public class Category
+{
+    public int Id { get; set; }
+    public string? Description { get; set; }
+}
---
+++ Models/Todo.cs
@@ -0,0 +1,27 @@
+﻿namespace Planner.Models;
+
+public enum TodoPriority
+{
+    Urgent = 1,
+    Important = 2,
+    Casual = 3
+}
+
+public class Todo
+{
+    //[Key] //Evidenciar de outra forma
+    public int Id { get; set; }
+
+    //[Required] //Evidenciar de outra forma
+    public DateTime CreationDate { get; set; }
+    public TodoPriority Priority { get; set; }
+
+    //[Required] //Evidenciar de outra forma
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public bool Done { get; set; }
+    public DateTime? DoneDate { get; set; }
+
+    public int CategoryId { get; set; }
+    public Category? Category { get; set; } 
+}
